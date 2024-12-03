5132b3df5478e0df2407f546c934af122290be84
1154472276@qq.com <1154472276@qq.com>
2024-12-03 09:32:27 +0800

    Revert "web11"
    This reverts commit c5a8b096ea2a14d519b6ed4c31a3c1cbbe4c0e1a.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+<<<<<<< HEAD
     return view('welcomef111');
+=======
+    return view('welcomeffgb33');
+>>>>>>> Revert "web11"
 });
