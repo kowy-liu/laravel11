f6ce6d3f5342cc831b33e7e0841c06c8666cbf68
1154472276@qq.com <1154472276@qq.com>
2024-12-03 10:13:23 +0800

    add suba

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-<<<<<<< HEAD
     return view('welcomef111');
-=======
-    return view('welcomeffgb33');
->>>>>>> Revert "web11"
 });
